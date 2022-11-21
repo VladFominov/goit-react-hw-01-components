dac61c0e9dae6f076c6874f05bbb66f65ac392ae
VladFominov <111357638+VladFominov@users.noreply.github.com>
2022-11-21 11:50:28 +0200

    react-hw-01-firstpush

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,20 @@
+import {
+  FriendItem,
+  FriendIsOnline,
+  FriendListImg,
+  FriendListName,
+  FriendIsOffline,
+} from '../FriendList/FriendList.styled';
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <FriendItem>
+      {isOnline ? (
+        <FriendIsOnline status={isOnline}>{isOnline}</FriendIsOnline>
+      ) : (
+        <FriendIsOffline status={isOnline}>{isOnline}</FriendIsOffline>
+      )}
+      <FriendListImg src={avatar} alt="User avatar" width="48" />
+      <FriendListName>{name}</FriendListName>
+    </FriendItem>
+  );
+};
---
+++ src/components/TransactionHistory/TransactionHistory.styled.jsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+
+export const TransactionTable = styled.table`
+  width: 100%;
+  margin: auto;
+  padding: 0 20px;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+`;
+
+export const TransactionThead = styled.thead`
+  text-align: center;
+  padding: 15px;
+  background-color: cornflowerblue;
+  color: white;
+  text-transform: uppercase;
+`;
+
+export const TransactionTbody = styled.tbody`
+  text-align: center;
+  padding: 10px;
+`;
+
+export const TransactionTr = styled.tr`
+  text-align: center;
+  padding: 10px;
+  transition: all 0.3s;
+  &:nth-child(odd) {
+    background: aliceblue;
+  }
+
+  &:hover {
+    background-color: coral;
+    transition: all 0.3s;
+  }
+`;
+
+export const TransactionTd = styled.td`
+  text-align: center;
+  padding: 25px;
+
+  &:not(:last-child) {
+    border-right: 1px solid grey;
+  }
+`;
